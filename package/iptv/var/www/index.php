3c8dcb3756a83b5a6ad7c0f6717f46f235619c89
Michaël Roy <microygh@gmail.com>
2018-04-02 12:38:54 +0200

    Start implementing a web admin interface.

---
+++ package/iptv/var/www/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>IPTV Server Admin</title>
+<link href="bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+<h1>Administration du service de streaming TV</h1>
+<div class="footer">
+<p>IUT RT Auxerre</p>
+</div><!-- footer -->
+</div> <!-- container -->
+</body>
+
+</html>
