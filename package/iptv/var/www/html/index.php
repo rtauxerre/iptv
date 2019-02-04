9ee792e06450dbc39c8736bf5cfa24fd309c5a5d
Michaël Roy <microygh@gmail.com>
2019-02-04 02:38:05 +0100

    Simplify the web page, and remove the functions.php file.

--- package/iptv/var/www/html/index.php
+++ package/iptv/var/www/html/index.php
@@ -9,16 +9,40 @@
 	<title>TV Streaming</title>
 </head>
 
-<?php require_once("functions.php"); ?>
+<?php
+if ( !empty($_GET) ) {
+	if ( isset($_GET["start"]) == TRUE ) system( 'sudo systemctl start iptv' );
+	elseif ( isset($_GET["restart"]) == TRUE ) system( 'sudo systemctl restart iptv' );
+	elseif ( isset($_GET["stop"]) == TRUE ) system( 'sudo systemctl stop iptv' );
+	elseif ( isset($_GET["shutdown"]) == TRUE ) system( 'sudo systemctl poweroff' );
+	header('Location: index.php');
+}
+?>
 
 <body>
 	<div class="container">
 		<h1>TV Streaming</h1>
 		<h2>Service control</h2>
-		<div class="container"><?php control(); ?></div>
+		<div class="container">
+			<a role="button" href="index.php?start" class="btn btn-success">Start</a>
+			<a role="button" href="index.php?restart" class="btn btn-warning">Restart</a>
+			<a role="button" href="index.php?stop" class="btn btn-danger">Stop</a>
+			<a role="button" href="index.php?shutdown" class="btn btn-secondary">Shutdown</a>
+		</div>
 		<br />
 		<h2>Service status</h2>
-		<div class="container"><?php status(); ?></div>
+		<div class="container">
+			<?php
+			echo '<pre>';
+			system( 'systemctl status iptv', $error );
+			echo '</pre>';
+			if ( $error == 0 ) echo '<div class="alert alert-success"><strong>Success!</strong> IPTV service is started</div>';
+			else echo '<div class="alert alert-danger"><strong>Failed!</strong> IPTV service is not started</div>';
+			echo '<pre>';
+			system( 'sudo journalctl -xb | grep \'dvblast\[\'' );
+			echo '</pre>';
+			?>
+		</div>
 	</div>
 </body>
 
